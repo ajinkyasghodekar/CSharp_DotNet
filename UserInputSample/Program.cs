4f5f5e315b21af0c8512fc6984917c8b3b198e1c
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-22 14:53:33 +0530

    Input from user and print in console.

--- UserInputSample/Program.cs
+++ UserInputSample/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System;
+
+namespace UserInput
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine();
+
+            string fname, mname, lname;
+            Console.Write("Enter your first name: ");
+            fname = Console.ReadLine();
+
+            Console.Write("Enter your middle name: ");
+            mname = Console.ReadLine();
+
+            Console.Write("Enter your last name: ");
+            lname = Console.ReadLine();
+
+            Console.WriteLine($"Your full name is {fname} {mname} {lname}");
+            
+            Console.WriteLine();
+        }
+    }
+}
