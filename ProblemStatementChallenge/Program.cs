cc7062ca34eb1e76e2de1d39893538543e6a66fa
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-22 20:40:51 +0530

    Solution to problem statement updated

--- ProblemStatementChallenge/Program.cs
+++ ProblemStatementChallenge/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using System;
+
+namespace Problem1
+{
+    class Program
+    {
+
+        // Problem Statement:
+        // Write C# code to find the sum of all integers 1 through 20 that are divisible by 3
+        // Using method call
+        static void Method1()
+        {
+            int sum = 0;
+            for (int i = 1; i < 20; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    sum = sum + i;
+
+                }
+            }
+            Console.WriteLine("The sum of all integers 1 through 20 that are divisible by 3 is: " +sum);
+        }
+        static void Main (string[] args)
+        {
+            // Method calling
+            Method1();
+        }
+    }
+}
