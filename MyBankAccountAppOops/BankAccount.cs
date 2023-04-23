41e1a87cd29a2a90a6ddb5774121fc5f11aa9a48
ajinkyasghodekar <ajinkyasghodekar@gmail.com>
2023-04-23 17:44:11 +0530

    Created 10 degit bank account number - 1.1

--- MyBankAccountAppOops/BankAccount.cs
+++ MyBankAccountAppOops/BankAccount.cs
@@ -7,19 +7,28 @@ namespace MyBankAccountApp
 
         public BankAccount(string name, decimal InitialBalance)
         {
+            // Step I
             this.Owner = name;
             this.Balance = InitialBalance;
+
+            // Step II
+            this.Number = AccountNumber.ToString();
+            AccountNumber++;
         }
         public string Number { get; }
         public string Owner { get; set; }
 
         public decimal Balance { get; set; }
 
+        private static int AccountNumber = 1234567890;
+
+        // Deposite Operations
         public void Deposit (decimal ammount, DateTime data, string note)
         {
 
         }
 
+        //Withdrawl operations
         public void Withdrawl(decimal ammount, DateTime data, string note)
         {
 
--- MyBankAccountAppOops/Program.cs
+++ MyBankAccountAppOops/Program.cs
@@ -6,9 +6,12 @@ namespace MyBankAccountApp
     {
         static void Main (string[] args)
         {
-            // Step I
-            var Account = new BankAccount ("Ajinkya", 10000);
-            Console.WriteLine($"A new bank account has been created for {Account.Owner}, with Account number {Account.Number} and Initial balance is {Account.Balance}");
+            // Step I 
+            var Account1 = new BankAccount ("Ajinkya", 10000);
+            Console.WriteLine($"A new bank account has been created for {Account1.Owner}, with Account number {Account1.Number} and Initial balance is {Account1.Balance}");
+
+            var Account2 = new BankAccount ("Ajay", 1000);
+            Console.WriteLine($"A new bank account has been created for {Account2.Owner}, with Account number {Account2.Number} and Initial balance is {Account2.Balance}");
         
         
         
